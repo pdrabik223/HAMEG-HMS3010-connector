f7f097aed7ad358419ec1df2491bfce75526ff38
piotr233 <pdrabik223@gmail.com>
2022-08-07 23:14:45 +0200

    working connection example

--- src/test.py
+++ src/test.py
@@ -1,49 +1,38 @@
+from time import sleep
 import usb.core
 import usb.util
 
-class UsbDev:
-    def __init__(self, VID, PID):
-        self.output_ep = None
-        self.input_ep = None
-        self.VID = VID
-        self.PID = PID
-        self.device = usb.core.find(idVendor = VID, idProduct = PID)
-        if self.device is None:
-            raise AttributeError('Failed to connect')
 
-    def check_device(self, dev_name=None):
-        if dev_name is None:
-            raise ValueError("device name provided is None")
-        if self.device.product != dev_name:
-            raise ValueError('Wrong type of product connected to host')
+def connect(vid, pid):
+    dev = usb.core.find(idVendor=vid, idProduct=pid)
+    if dev is None:
+        raise ValueError("Device is not found")
 
-    def config_device(self):
-        self.device.set_configuration()
-        cfg = self.device.get_active_configuration()
-        self.output_ep = usb.util.find_descriptor(cfg[(0, 0)], custom_match=lambda e:
-            usb.util.endpoint_direction(e.bEndpointAddress) == usb.util.ENDPOINT_OUT)
-        self.input_ep = usb.util.find_descriptor(cfg[(0, 0)], custom_match=lambda e:
-            usb.util.endpoint_direction(e.bEndpointAddress) == usb.util.ENDPOINT_IN)
+    dev.set_configuration()
+    return dev
 
-    def read_string(self):
-        if self.input_ep is not None:
-            ret_array = self.device.read(self.input_ep.bEndpointAddress,
-                                      self.input_ep.wMaxPacketSize)
-            self.device.clear_halt(self.input_ep)
-            return ''.join([chr(x) for x in ret_array])  # always returns 1
-        
-    def send(self, payload :str)->None:
-        self.device.write(self.input_ep.bEndpointAddress, payload)        
-        
-        
-        
-        
-        
 
-driver = UsbDev(0x0403, 0xed72)  
-# driver.check_device('HAMEG HMS3010')
-print(f"HAMEG device info:\n{driver.device}")
-driver.config_device()
-print(driver.input_ep.bEndpointAddress)
-driver.send("*IDN?\n")    
-print(driver.read_string())
+cmd = "*ESE?\n"
+
+
+def send(dev, cmd: str):
+    if len(command) != 0:
+        cmd += "\n"
+        dev.write(0x2, cmd)
+        sleep(0.5)
+    resp = dev.read(0x81, 1_000_000, 1000)
+    try:
+        decoded = bytearray(resp).decode("utf-8")
+    except Exception as ex:
+        return (resp, f"fail with error message: {str(ex)}")
+    return (resp, decoded)
+
+
+if __name__ == "__main__":
+    device = connect(0x0403, 0xED72)
+
+    while True:
+        command = input("command: ")
+        resp = send(device, command)
+        print(f"response: {resp[0]}")
+        print(f"          {resp[1]}")
