1e0427836ebae497ea9135603e2833e126101aa7
piotr233 <pdrabik223@gmail.com>
2022-08-28 16:51:50 +0200

    unit tests and integration tests work

--- src/test.py
+++ src/test.py
@@ -6,8 +6,13 @@ from device import Device
 if __name__ == "__main__":
     dev = Device.connect_using_vid_pid(idVendor=0x0403, idProduct=0xED72)
 
+    print("current date:")
     print(dev.send_await_resp(system.Date.receive()))
+
+    print("set date to NOW!")
     dev.send_await_resp(system.Time.send(datetime.now()))
+
+    print("current date:")
     print(dev.send_await_resp(system.Time.receive()))
 
     # while True:
