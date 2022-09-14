31275c0c7c8f712e8f2833b748f1c57ef94291a4
piotr233 <pdrabik223@gmail.com>
2022-09-14 18:25:24 +0200

    tests

--- src/test.py
+++ src/test.py
@@ -3,21 +3,16 @@ from requests import system
 from device import Device
 
 
-if __name__ == "__main__":
+def console_ui():
+    dev = Device.connect_using_vid_pid(idVendor=0x0403, idProduct=0xED72)
+    while True:
+        command = input("command: ")
+        resp = dev.send_await_resp(command)
+        print(f"response: {resp[0]}")
+        print(f"          {resp[1]}")
+def simple_ui():
     dev = Device.connect_using_vid_pid(idVendor=0x0403, idProduct=0xED72)
 
-    print(f"current date: {system.Date.receive()._send()}")
-    print(dev.send_await_resp(system.Date.receive()))
 
-    print(f"set date to NOW!: {system.Date.send(datetime.now())._send()}")
-    dev.send_await_resp(system.Date.send(datetime.now()))
-
-    print(f"current date:") #{system.Date.receive()._send()}
-    # print(f"resp: {dev.send_await_resp(system.Date.receive()._send())}")
-    print(dev.send_await_resp(system.Date.receive()))
-
-    # while True:
-    #     command = input("command: ")
-    #     resp = dev.send_await_resp(command)
-    #     print(f"response: {resp[0]}")
-    #     print(f"          {resp[1]}")
+if __name__ == "__main__":
+    console_ui()
